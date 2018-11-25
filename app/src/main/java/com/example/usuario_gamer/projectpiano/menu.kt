dac6b9342a8e9b05b22479ca0506ddff9702b3c1
TomSanchez <41757207+TomSanchez@users.noreply.github.com>
2018-11-25 19:41:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/usuario_gamer/projectpiano/menu.kt
@@ -0,0 +1,12 @@
+package com.example.usuario_gamer.projectpiano
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class menu : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
